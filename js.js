bc635a9338f2b21f50bd91f7ab67a97537e7782d
MD. MONIRUZZAMAN <53488876+zamanmonirbu@users.noreply.github.com>
2023-01-10 20:47:49 +0600

    first commit

---
+++ js.js
@@ -0,0 +1,10 @@
+// fetch('https://restcountries.com/v2/all')
+// .then(req=>req.json())
+// .then(data=>displayCountries(data));
+//     const displayCountries=countries=>{
+//             countries.forEach(country => {
+         
+//             console.log(country.name);
+          
+//         });
+//     }

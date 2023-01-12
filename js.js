32846f87911a92f6eb009f545773f598c7662069
MD. MONIRUZZAMAN <monir.cse6.bu@gmail.com>
2023-01-12 14:44:52 +0600

    uthso

--- js.js
+++ js.js
@@ -15,3 +15,25 @@ foodb.forEach(foodb=>{
     // console.log(category.strMeal); 
     
 });}
+
+const searchResult=data=>{
+    const input=document.getElementById('input').value;
+    //const url=`www.themealdb.com/api/json/v1/1/search.php?s=${input}`;
+    const url=`https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata`;
+    fetch(url)
+    .then(req=>req.json())
+    .then(data=>clickData(data.meals[0]))
+}
+
+const clickData=data=>{
+    //console.log(data);
+    
+    const newDiv=document.createElement('div');
+    newDiv.className='foodName';
+    newDiv.innerHTML=`<h1>${"Food Name is",data.strMeal}</h1>
+    <img src="${data.strMealThumb}">
+    `;
+    console.log(data.strMealThumb);
+
+    searchContent.appendChild(newDiv);
+}
